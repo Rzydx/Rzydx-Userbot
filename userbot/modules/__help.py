6e068d689aedd93f7738cb31f4138f8b04ac4d6b
Rizky_Yudha <102364151+Rzydx@users.noreply.github.com>
2022-03-26 14:22:43 +0700

    Update __help.py

--- userbot/modules/__help.py
+++ userbot/modules/__help.py
@@ -20,25 +20,25 @@
 
 
 from userbot import BOT_USERNAME, CMD_HELP, bot
-from userbot.utils import edit_or_reply, edit_delete, kyy_cmd
+from userbot.utils import edit_or_reply, edit_delete, rzydx_cmd
 
 user = bot.get_me()
 DEFAULTUSER = user.first_name
-CUSTOM_HELP_EMOJI = "✘"
+CUSTOM_HELP_EMOJI = "🔥"
 
 
-@kyy_cmd(pattern="help ?(.*)")
+@rzydx_cmd(pattern="help ?(.*)")
 async def cmd_list(event):
     args = event.pattern_match.group(1).lower()
     if args:
         if args in CMD_HELP:
-            await edit_or_reply(event, f"**✘ Commands available in {args} ✘** \n\n" + str(CMD_HELP[args]) + "\n\n**☞ @NastyProject**")
+            await edit_or_reply(event, f"**🔥 Commands available in {args} 🔥** \n\n" + str(CMD_HELP[args]) + "\n\n**☞ @Aksara_Rassa**")
         else:
             await edit_delete(event, f"**Module** `{args}` **Tidak tersedia!**")
     else:
         try:
             results = await bot.inline_query(  # pylint:disable=E0602
-                BOT_USERNAME, "@KyyUserbot"
+                BOT_USERNAME, "@RzydxUserbot"
             )
             await results[0].click(
                 event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
