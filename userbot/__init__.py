10b066cf4317b2b81868e6637f7b5ab5fc3ec9e5
Rzydx <rizkyyudha0023@gmail.com>
2022-03-28 08:10:16 +0000

    pylint: auto fixes
    Signed-off-by: Rzydx <rizkyyudha0023@gmail.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -661,7 +661,8 @@ with bot:
             builder = event.builder
             result = None
             query = event.text
-            if event.query.user_id == uid and query.startswith("@RzydxUserbot"):
+            if event.query.user_id == uid and query.startswith(
+                    "@RzydxUserbot"):
                 buttons = paginate_help(0, dugmeler, "helpme")
                 result = await event.builder.photo(
                     file=rzydxlogo,
