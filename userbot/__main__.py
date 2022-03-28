676e791a0c1f1166a0f15882074bb624e7f80ecf
Rizky_Yudha <102364151+Rzydx@users.noreply.github.com>
2022-03-28 14:18:13 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -57,7 +57,7 @@ LOGS.info(
 async def check_alive():
     try:
         if BOTLOG_CHATID != 0:
-            await bot.send_message(BOTLOG_CHATID, "♨️ **Rzydx Userbot Udah Hidup Ya Kontol**!!\n━━━━━━━━━━━━━━━\n🔥 **Userbot Version** - 3.1.5 @Rzydx_Userbot\n🔥 **Ketik** `.ping` **Buat Ngecheck Bot Lu Tod**\n━━━━━━━━━━━━━━━\n🔥 **Powered By:** @RzydxProject ")
+            await bot.send_message(BOTLOG_CHATID, "•••╼═⍟═♨️ʀᴢʏᴅx-ᴜsᴇʀʙᴏᴛ♨️═⍟═╾•••\n╒ ➠ ৯• **ᴜsᴇʀʙᴏᴛ ᴠᴇʀsɪᴏɴ** - 3.1.5 @Rzydx_Userbot\n╞ ➠ ৯• **ᴋᴇᴛɪᴋ** `.ping` **ʙᴜᴀᴛ ɴɢᴇᴄᴇᴋ ʙᴏᴛ ʟᴜ ᴛᴏᴅ**\n╘ ➠ ৯• **ᴘᴏᴡᴇʀᴇᴅ ʙʏ** @RzydxProject\n•••••╼══════⍟══════╾•••••\n🔥 **ᴜsᴇʀʙᴏᴛ ʙᴇʀʜᴀsɪʟ ᴅɪᴀᴋᴛɪꜰᴋᴀɴ** 🔥 ")
     except Exception as e:
         LOGS.info(str(e))
     try:
