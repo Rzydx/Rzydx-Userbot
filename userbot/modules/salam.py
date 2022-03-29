7734083900f42e57bcd3a13894e8c80235207d65
Rizky_Yudha <102364151+Rzydx@users.noreply.github.com>
2022-03-29 14:40:17 +0700

    Update salam.py

--- userbot/modules/salam.py
+++ userbot/modules/salam.py
@@ -1,31 +1,31 @@
 from platform import uname
 from userbot import ALIVE_NAME, CMD_HELP, CMD_HANDLER as cmd
-from userbot.utils import kyy_cmd
+from userbot.utils import rzydx_cmd
 
 # ================= CONSTANT =================
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
 
 
-@kyy_cmd(pattern='^P(?: |$)(.*)')
+@rzydx_cmd(pattern='^P(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("𝐀𝐬𝐬𝐚𝐥𝐚𝐦𝐮'𝐚𝐥𝐚𝐢𝐤𝐮𝐦...")
 
 
-@kyy_cmd(pattern='atg(?: |$)(.*)')
+@rzydx_cmd(pattern='atg(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("𝐀𝐒𝐓𝐀𝐆𝐇𝐅𝐈𝐑𝐔𝐋𝐋𝐀𝐇....SAYANG!!!!")
 
 
-@kyy_cmd(pattern='^L(?: |$)(.*)')
+@rzydx_cmd(pattern='^L(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("𝐖𝐚'𝐚𝐥𝐚𝐢𝐤𝐮𝐦𝐬𝐚𝐥𝐚𝐦...")
 
 
-@kyy_cmd(pattern='ast(?: |$)(.*)')
+@rzydx_cmd(pattern='ast(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("𝐀𝐒𝐓𝐀𝐆𝐇𝐅𝐈𝐑𝐔𝐋𝐋𝐀𝐇......")
