739e207af9b89ef5c7d0f3f7cbb381c8c8bf6f14
Rizky_Yudha <rizkyyudha0023@gmail.com>
2022-03-30 06:22:43 +0700

    last

--- userbot/modules/__help.py
+++ userbot/modules/__help.py
@@ -18,13 +18,25 @@
 #    From Flicks-Userbot
 #    <t.me/TheFlicksUserbot>
 
+import os
 
-from userbot import BOT_USERNAME, CMD_HELP, bot
-from userbot.utils import edit_or_reply, edit_delete, rzydx_cmd
+from telethon import Button
+from userbot import ALIVE_NAME, BOT_USERNAME, CMD_HELP, CMD_HANDLER, CMD_LIST, bot, tgbot
+from userbot.utils import rzydx_cmd
 
 user = bot.get_me()
 DEFAULTUSER = user.first_name
-CUSTOM_HELP_EMOJI = "⍣"
+CUSTOM_HELP_EMOJI = "⚡"
+main_help_menu = [
+    [
+        Button.url("Settings ⚙️", f"t.me/{BOT_USERNAME}"),
+        Button.inline("Vc Plugin ⚙️", data="rzydx_inline"),
+    ],
+    [
+        Button.inline("Help Menu", data="open"),
+    ],
+    [Button.inline("Close", data="close")],
+]
 
 
 @rzydx_cmd(pattern="help ?(.*)")
@@ -32,19 +44,19 @@ async def cmd_list(event):
     args = event.pattern_match.group(1).lower()
     if args:
         if args in CMD_HELP:
-            await edit_or_reply(event, f"**⍣ Commands available in {args} ⍣** \n\n" + str(CMD_HELP[args]) + "\n\n**🔥 @RzydxProject**")
+            await event.edit(f"**✘ Commands available in {args} ✘** \n\n" + str(CMD_HELP[args]) + "\n\n**💕 @RzydxProject**")
         else:
-            await edit_delete(event, f"**Module** `{args}` **Tidak tersedia!**")
+            await event.edit(f"**Module** `{args}` **Tidak tersedia!**")
     else:
         try:
             results = await bot.inline_query(  # pylint:disable=E0602
-                BOT_USERNAME, "@Rzydx_Userbot"
+                BOT_USERNAME, "@Rzydx_Support"
             )
             await results[0].click(
                 event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
             )
             await event.delete()
         except BaseException:
-            await edit_delete(event,
-                              f"** Sepertinya obrolan atau bot ini tidak mendukung inline mode.**"
-                              )
+            await event.edit(
+                f"** Sepertinya obrolan atau bot ini tidak mendukung inline mode.**"
+            )
