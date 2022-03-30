712b824e6dfe7c266f46f309783e33e761ce6950
Rizky_Yudha <rizkyyudha0023@gmail.com>
2022-03-30 15:33:35 +0700

    last

--- string_session.py
+++ string_session.py
@@ -19,7 +19,9 @@ API_HASH = input("Enter API_HASH here: ")
 with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
     print("Check Telegram Save Message Mu Untuk Copy STRING_SESSION ")
     session_string = client.session.save()
-    saved_messages_template = """😏 Hatur nuhun parantos Support Rzydx-Userbot | Channel: @RzydxProject
+    saved_messages_template = """😼 Thank u for Support ⭐Rzydx-Userbot ⭐ | Grup @Rzydx_Support
+
 <code>STRING_SESSION</code>: <code>{}</code>
+
 ⚠️ <i>Please be careful before passing this value to third parties</i>""".format(session_string)
     client.send_message("me", saved_messages_template, parse_mode="html")
